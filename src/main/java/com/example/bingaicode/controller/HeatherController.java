e8892345abf8f8b33e1c74a1df7f12f4caa9d216
bingbingedu <1813236997@qq.com>
2026-03-31 11:01:24 +0800

    后端初始化 1.初始化 2.添加依赖 3.提供基础代码 4.提供基础接口

---
+++ src/main/java/com/example/bingaicode/controller/HeatherController.java
@@ -0,0 +1,17 @@
+package com.example.bingaicode.controller;
+
+import com.example.bingaicode.common.BaseResponse;
+import com.example.bingaicode.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HeatherController {
+
+    @GetMapping("/")
+    public BaseResponse<String> heathCheck() {
+        return ResultUtils.success("ok!");
+    }
+}
---
+++ src/test/java/com/example/bingaicode/BingAiCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bingaicode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BingAiCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
