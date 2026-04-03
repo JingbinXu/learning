6f4a85917a2565f0364a30ad4ca732bb4078258b
bingbingedu <1813236997@qq.com>
2026-04-03 15:43:00 +0800

    完成基本功能：AI生成基础功能代码

--- src/main/java/com/bing/bingaicode/mapper/AppMapper.java
+++ src/main/java/com/bing/bingaicode/mapper/AppMapper.java
@@ -1,12 +1,12 @@
 package com.bing.bingaicode.mapper;
 
-import com.mybatisflex.core.BaseMapper;
 import com.bing.bingaicode.model.entity.App;
+import com.mybatisflex.core.BaseMapper;
 
 /**
  * 应用 映射层。
  *
- * @author bing
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
  */
 public interface AppMapper extends BaseMapper<App> {
 
--- src/main/java/com/bing/bingaicode/service/AppService.java
+++ src/main/java/com/bing/bingaicode/service/AppService.java
@@ -1,13 +1,46 @@
 package com.bing.bingaicode.service;
 
-import com.mybatisflex.core.service.IService;
+import com.bing.bingaicode.model.app.AppAddRequest;
+import com.bing.bingaicode.model.app.AppQueryRequest;
 import com.bing.bingaicode.model.entity.App;
+import com.bing.bingaicode.model.entity.User;
+import com.bing.bingaicode.model.vo.AppVO;
+import com.mybatisflex.core.query.QueryWrapper;
+import com.mybatisflex.core.service.IService;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
 
 /**
  * 应用 服务层。
  *
- * @author bing
+ *
  */
 public interface AppService extends IService<App> {
+
+
+    /**
+     * 获取应用封装类
+     *
+     * @param app
+     * @return
+     */
+    AppVO getAppVO(App app);
+
+    /**
+     * 获取应用封装类列表
+     *
+     * @param appList
+     * @return
+     */
+    List<AppVO> getAppVOList(List<App> appList);
+
+    /**
+     * 构造应用查询条件
+     *
+     * @param appQueryRequest
+     * @return
+     */
+    QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
 
 }
